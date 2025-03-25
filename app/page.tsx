c2e51f7e355e7a0ae592ebd8ac3df722168f3444
John Nguyen <jnguyen2575@gmail.com>
2025-03-24 21:03:36 -0400

    fixed scrollable section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Filter from "@/components/Filter";
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Header from "@/components/Header";
